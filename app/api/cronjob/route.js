49657abdcdcb03a6a1df4bf253bee3bf3bda6298
AdepojuWasiu <adepojuwasiu031@gmail.com>
2024-09-12 19:12:12 +0100

    fix typo

---
+++ app/api/cronjob/route.js
@@ -0,0 +1,17 @@
+import User from "@/models/user";
+import { connectToDB } from "@/utils/database";
+
+export const POST = async (request) => {
+    try {
+        // Connect to the database
+        await connectToDB();
+
+        // Update availableTurbo to 5 for all users
+        await User.updateMany({}, { availableTurbo: 5, availableEnergyRefill: 5});
+
+        return new Response("Turbo updated for all users", { status: 200 });
+
+    } catch (error) {
+        return new Response("Failed to update users", { status: 500 });
+    }
+};
