142c2405d8d684a29fb84e9c4e4f1c5388fe83e5
AdepojuWasiu <adepojuwasiu031@gmail.com>
2024-09-14 22:47:33 +0100

    fix typo

--- app/page.jsx
+++ app/page.jsx
@@ -146,53 +146,27 @@ const Home = () => {
     //   };
     // }, [onClose]);
 
-    // const updatePointWithBeacon =  () => {
-    //   const url = "/api/update";
-    //   const data = JSON.stringify({
-    //     userId: userid,
-    //     point: points,
-    //   });
-  
-    //   navigator.sendBeacon(url, data);
-    // };
-  
-    // useEffect(() => {
-    //    Telegram.WebApp.ready();
-    //    Telegram.WebApp.onEvent('close', () => {
-    //     updatePointWithBeacon();
-    //     Telegram.WebApp.close();
-    //    });
-    //    return () => {
-    //     Telegram.WebApp.offEvent('close');
-    //    };
-       
-    // }, []);
-
-    const updatePointWithBeacon = () => {
+    const updatePointWithBeacon =  () => {
       const url = "/api/update";
-      const data = new Blob([JSON.stringify({
+      const data = JSON.stringify({
         userId: userid,
         point: points,
-      })], { type: 'application/json' });
-      
+      });
+  
       navigator.sendBeacon(url, data);
     };
-    
+  
     useEffect(() => {
        Telegram.WebApp.ready();
        Telegram.WebApp.onEvent('close', () => {
         updatePointWithBeacon();
-        // Delay closing the web app to allow the beacon to send
-        setTimeout(() => {
-          Telegram.WebApp.close();
-        }, 100); // Adjust timeout as needed
+        Telegram.WebApp.close();
        });
-       
        return () => {
         Telegram.WebApp.offEvent('close');
        };
-    }, [points]);
-    
+       
+    }, []);
   
 
   return (
