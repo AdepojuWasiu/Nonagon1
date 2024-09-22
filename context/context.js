e7d3d03354cf3f4301b601534a048a98539f7cd1
AdepojuWasiu <adepojuwasiu031@gmail.com>
2024-09-22 01:08:42 +0100

    fix typo

--- context/context.js
+++ context/context.js
@@ -79,7 +79,7 @@ useEffect(() => {
                     const timeDifferent =  timeLogin - lastEnergyTime;
                     const addEnergy =  data.energyIncrease * timeDifferent;
                     const newEnergy =  data.energy + addEnergy;
-                    alert(timeDifferent)
+                    alert(timeLogin  data.lastEnergyUpdatedTime)
                     if(newEnergy >= data.energyLimit) {
                       setEnergy(data.energyLimit);
                     } else {
