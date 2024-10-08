01f7eeb52af4f48d53a2b89a5cb3044b82fc3444
AdepojuWasiu <adepojuwasiu031@gmail.com>
2024-10-08 15:57:00 +0100

    fix typo

--- app/page.jsx
+++ app/page.jsx
@@ -191,8 +191,8 @@ const Home = () => {
                     key={click.id}
                     className="absolute text-[30px] font-bold text-[#fff]"
                     style={{
-                      top: `${click.y - 65}px`,
-                      left: `${click.x - 15}px`,
+                      top: `${click.y - 0}px`,
+                      left: `${click.x - 0}px`,
                       animation: `float 1s ease forwards`,
                     }}
                     onAnimationEnd={() => handleAnimationEnd(click.id)}
