3d80cdd65b70374eada0088ba94147569da79b36
AdepojuWasiu <adepojuwasiu031@gmail.com>
2024-10-16 22:44:46 +0100

    fix typo

--- app/wallet/page.jsx
+++ app/wallet/page.jsx
@@ -8,7 +8,7 @@ const Wallet = () => {
     const {points} = useEnergy();
 
   return (
-    <div className="flex-col justify-center items-center justify-items-center">
+    <div className="flex-col justify-center items-center justify-items-center p-3">
               <div className="flex justify-center items-center justify-items-center content-center mt-5">
         <h1 className="font-bold text-[25px]">Your Balance</h1>
         </div>
@@ -16,7 +16,7 @@ const Wallet = () => {
             <Image src="/assets/coin.jpg" alt="coin" width={40} height={40} className="rounded-full"/>
             <p className="text-[30px] font-bold">{points.toLocaleString()}</p>
         </div>
-        <div  className="bg-[#ffff] text-[#000] flex justify-center justify-items-center font-bold text-[20px] p-4 px-8 rounded-xl gap-2">
+        <div  className="bg-[#ffff] text-[#000] flex justify-center justify-items-center font-bold text-[20px] p-4 px-8 rounded-xl gap-6 mt-[200px]">
               <p>Connect Wallet </p>
               <p className="text-[15px] text-gray-600">soon</p>
          </div>
