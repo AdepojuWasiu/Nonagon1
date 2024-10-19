0cc75fce22b7268473a451748f0969f0bbde384e
AdepojuWasiu <adepojuwasiu031@gmail.com>
2024-10-19 20:02:18 +0100

    fix typo

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
+    reactStrictMode: true,
     experimental: {
       serverComponentsExternalPackages: ["mongoose"],
       // missingSuspenseWithCSRBailout: false,
