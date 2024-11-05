ebf9e43f12dff8e61da8ac7b3812e7b236a12c04
AdepojuWasiu <adepojuwasiu031@gmail.com>
2024-11-05 18:19:55 +0100

    fix typo

--- app/level/page.jsx
+++ app/level/page.jsx
@@ -1,9 +1,150 @@
+
 import React from 'react'
+import { FaHandshake } from "react-icons/fa";
 
 const Level = () => {
   return (
     <div>
-      Level
+          <div className="bg-[#272727] flex  rounded-md pb-2 pt-2 pl-4 pr-4 mt-4 justify-between ">
+                <div className="flex gap-4">
+                  <div className="bg-[#000] py-3 px-5 rounded-full font-bold text-[15px] text-[#ffbf00]"><p>1</p></div>
+                  <div>
+                  <p className="text-[17px] font-bold">Circle</p>
+                  <div className="flex gap-2">
+                      <p>10,000 - 200,000</p>
+                  </div>
+                  </div>
+               </div>
+               <div className="flex mt-3">
+                 <FaHandshake className="w-[30px] h-[30px]" color="gold" />
+              </div>
+         </div>
+         <div className="bg-[#272727] flex  rounded-md pb-2 pt-2 pl-4 pr-4 mt-4 justify-between ">
+                <div className="flex gap-4">
+                  <div className="bg-[#000] py-3 px-5 rounded-full font-bold text-[15px] text-[#ffbf00]"><p>1</p></div>
+                  <div>
+                  <p className="text-[17px] font-bold">Circle</p>
+                  <div className="flex gap-2">
+                      <p>10,000 - 200,000</p>
+                  </div>
+                  </div>
+               </div>
+               <div className="flex mt-3">
+                 <FaHandshake className="w-[30px] h-[30px]" color="gold" />
+              </div>
+         </div>
+         <div className="bg-[#272727] flex  rounded-md pb-2 pt-2 pl-4 pr-4 mt-4 justify-between ">
+                <div className="flex gap-4">
+                  <div className="bg-[#000] py-3 px-5 rounded-full font-bold text-[15px] text-[#ffbf00]"><p>1</p></div>
+                  <div>
+                  <p className="text-[17px] font-bold">Circle</p>
+                  <div className="flex gap-2">
+                      <p>10,000 - 200,000</p>
+                  </div>
+                  </div>
+               </div>
+               <div className="flex mt-3">
+                 <FaHandshake className="w-[30px] h-[30px]" color="gold" />
+              </div>
+         </div>
+         <div className="bg-[#272727] flex  rounded-md pb-2 pt-2 pl-4 pr-4 mt-4 justify-between ">
+                <div className="flex gap-4">
+                  <div className="bg-[#000] py-3 px-5 rounded-full font-bold text-[15px] text-[#ffbf00]"><p>1</p></div>
+                  <div>
+                  <p className="text-[17px] font-bold">Circle</p>
+                  <div className="flex gap-2">
+                      <p>10,000 - 200,000</p>
+                  </div>
+                  </div>
+               </div>
+               <div className="flex mt-3">
+                 <FaHandshake className="w-[30px] h-[30px]" color="gold" />
+              </div>
+         </div>
+         <div className="bg-[#272727] flex  rounded-md pb-2 pt-2 pl-4 pr-4 mt-4 justify-between ">
+                <div className="flex gap-4">
+                  <div className="bg-[#000] py-3 px-5 rounded-full font-bold text-[15px] text-[#ffbf00]"><p>1</p></div>
+                  <div>
+                  <p className="text-[17px] font-bold">Circle</p>
+                  <div className="flex gap-2">
+                      <p>10,000 - 200,000</p>
+                  </div>
+                  </div>
+               </div>
+               <div className="flex mt-3">
+                 <FaHandshake className="w-[30px] h-[30px]" color="gold" />
+              </div>
+         </div>
+         <div className="bg-[#272727] flex  rounded-md pb-2 pt-2 pl-4 pr-4 mt-4 justify-between ">
+                <div className="flex gap-4">
+                  <div className="bg-[#000] py-3 px-5 rounded-full font-bold text-[15px] text-[#ffbf00]"><p>1</p></div>
+                  <div>
+                  <p className="text-[17px] font-bold">Circle</p>
+                  <div className="flex gap-2">
+                      <p>10,000 - 200,000</p>
+                  </div>
+                  </div>
+               </div>
+               <div className="flex mt-3">
+                 <FaHandshake className="w-[30px] h-[30px]" color="gold" />
+              </div>
+         </div>
+         <div className="bg-[#272727] flex  rounded-md pb-2 pt-2 pl-4 pr-4 mt-4 justify-between ">
+                <div className="flex gap-4">
+                  <div className="bg-[#000] py-3 px-5 rounded-full font-bold text-[15px] text-[#ffbf00]"><p>1</p></div>
+                  <div>
+                  <p className="text-[17px] font-bold">Circle</p>
+                  <div className="flex gap-2">
+                      <p>10,000 - 200,000</p>
+                  </div>
+                  </div>
+               </div>
+               <div className="flex mt-3">
+                 <FaHandshake className="w-[30px] h-[30px]" color="gold" />
+              </div>
+         </div>
+         <div className="bg-[#272727] flex  rounded-md pb-2 pt-2 pl-4 pr-4 mt-4 justify-between ">
+                <div className="flex gap-4">
+                  <div className="bg-[#000] py-3 px-5 rounded-full font-bold text-[15px] text-[#ffbf00]"><p>1</p></div>
+                  <div>
+                  <p className="text-[17px] font-bold">Circle</p>
+                  <div className="flex gap-2">
+                      <p>10,000 - 200,000</p>
+                  </div>
+                  </div>
+               </div>
+               <div className="flex mt-3">
+                 <FaHandshake className="w-[30px] h-[30px]" color="gold" />
+              </div>
+         </div>
+         <div className="bg-[#272727] flex  rounded-md pb-2 pt-2 pl-4 pr-4 mt-4 justify-between ">
+                <div className="flex gap-4">
+                  <div className="bg-[#000] py-3 px-5 rounded-full font-bold text-[15px] text-[#ffbf00]"><p>1</p></div>
+                  <div>
+                  <p className="text-[17px] font-bold">Circle</p>
+                  <div className="flex gap-2">
+                      <p>10,000 - 200,000</p>
+                  </div>
+                  </div>
+               </div>
+               <div className="flex mt-3">
+                 <FaHandshake className="w-[30px] h-[30px]" color="gold" />
+              </div>
+         </div>
+         <div className="bg-[#272727] flex  rounded-md pb-2 pt-2 pl-4 pr-4 mt-4 justify-between ">
+                <div className="flex gap-4">
+                  <div className="bg-[#000] py-3 px-5 rounded-full font-bold text-[15px] text-[#ffbf00]"><p>1</p></div>
+                  <div>
+                  <p className="text-[17px] font-bold">Circle</p>
+                  <div className="flex gap-2">
+                      <p>10,000 - 200,000</p>
+                  </div>
+                  </div>
+               </div>
+               <div className="flex mt-3">
+                 <FaHandshake className="w-[30px] h-[30px]" color="gold" />
+              </div>
+         </div>
     </div>
   )
 }
